aef53e20191d3fb0d7a1ef5374b8d83ecba9e5fc
khalil <khalilelqaddabi9@gmail.com>
2026-01-15 12:03:54 +0100

    SS-29 #done feat:set_up_database_connection

---
+++ src/DataBase.php
@@ -0,0 +1,15 @@
+<?php
+class Database {
+    private static ?\PDO $pdo =null;
+
+    public static function getconnection(): \PDO {
+        if (self::$pdo ===null) {
+            $config =require __DIR__ ."/../config/DataBase.php";
+            $dsn = "myssql:host={$config['host']};dbname={$config['dbname']};charset{$config['charset']}";
+
+            self::$pdo =new \PDO($dsn, $config['user'], $config['password'],[\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,]);
+        }
+        return self::$pdo;
+    }
+}
